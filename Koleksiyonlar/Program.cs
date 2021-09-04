8a4507b5311db2294c99ccf9bf0b4336fdb51b56
Hidayet ERDOĞAN <Hidayet ERDOAN@DESKTOP-HOJ60CC>
2021-09-04 17:50:25 +0300

    Proje dosyası ekle.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Büşra", "Hidayet","Berkay","Eda" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "Sena";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Büşra", "Hidayet", "Berkay", "Eda" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Sena");
+            isimler2.Add("Yasin");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[5]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
